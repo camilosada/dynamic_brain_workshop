03774d42f345a9c25751379aa00434874bd2dc2c
camilosada <camila.losada-gomez@univ-amu.fr>
2025-09-04 16:20:22 +0000

    + Added new analysis

--- code/src/bci/dataviz/traces.py
+++ code/src/bci/dataviz/traces.py
@@ -3,6 +3,7 @@ from pathlib import Path
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def plot_lick_spout_steps_dff(dff_signal:np.ndarray,zaber_steps:np.ndarray,frames_before:int,idx_threshold_change:List,subject_id:str,session_name:str,i_roi:int,cn:bool,colors:List,save:bool=False, savepath:str='/scratch',save_format:str='jpg'):
     """
     Plot delta F/F (dF/F) signals and lick spout steps across trials.
@@ -51,30 +52,37 @@ def plot_lick_spout_steps_dff(dff_signal:np.ndarray,zaber_steps:np.ndarray,frame
         y_space = y_space[:-1]
     dff_signal = dff_signal + y_space.reshape(-1,1)
     
-    fig,ax = plt.subplots(figsize=(20,15))
-    _=plt.plot(dff_signal[:,:frames_before+100].T,color='dimgrey')
-    _=plt.scatter(zaber_steps,np.zeros(zaber_steps.shape)+y_space.reshape(-1,1),color='b',marker='|')
+    fig,ax = plt.subplots(1,2,figsize=(20,15),width_ratios=[4, 1],sharey=True)
 
-    ax.vlines(frames_before, 0,ntrials*max_act, color='grey', label = 'Reward')
+    _=ax[0].plot(dff_signal[:,:frames_before+100].T,color='dimgrey')
+    _=ax[0].scatter(zaber_steps,np.zeros(zaber_steps.shape)+y_space.reshape(-1,1),color='b',marker='|')
+
+    ax[0].vlines(frames_before, 0,ntrials*max_act, color='grey', label = 'Reward')
     
     # Add colored bands for different threshold periods
     for iidx in range(len(idx_threshold_change)-1):
-        ax.axhspan(ymin=idx_threshold_change[iidx]*max_act, 
+        ax[0].axhspan(ymin=idx_threshold_change[iidx]*max_act, 
                   ymax=idx_threshold_change[iidx+1]*max_act,
                   xmin=0, xmax=frames_before, 
                   color=colors[iidx], alpha=0.1, 
                   label=f'Threshold {iidx+1}')
 
-    ax.set_xticks([frames_before-100,frames_before])
-    ax.set_xticklabels(['-100','0'])
-    ax.set_yticks(y_space)
-    ax.set_yticklabels(np.arange(1,ntrials+1))
-    ax.set_xlabel('Frames aligned on threshold crossing time')
-    ax.set_ylabel('Trials')
+    diff_steps=np.abs(np.diff(zaber_steps,axis=1))
+    mean_steps = np.nanmean(diff_steps,axis=1)
+    std_steps = np.nanstd(diff_steps,axis=1)
+
+    ax[1].errorbar(mean_steps,np.arange(0,ntrials*max_act,max_act), xerr=std_steps, color='r',fmt='-o', capsize=4, label='Data with Std Dev')
+
+    ax[0].set_xticks([frames_before-100,frames_before])
+    ax[0].set_xticklabels(['-100','0'])
+    ax[0].set_yticks(y_space)
+    ax[0].set_yticklabels(np.arange(1,ntrials+1))
+    ax[0].set_xlabel('Frames aligned on threshold crossing time')
+    ax[0].set_ylabel('Trials')
     if cn:
         i_roi = str(i_roi)+'CN'
-    ax.set_title(f'ROI nº{i_roi}')
-
+    ax[0].set_title(f'ROI nº{i_roi}')
+    plt.subplots_adjust(wspace=0, hspace=0)
     if save:
         subdir = f'{subject_id}/{session_name}'
         output = Path(f'{savepath}/{subdir}')
